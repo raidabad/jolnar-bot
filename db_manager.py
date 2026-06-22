f5a787f6aff0fb5aced689db0ab055a78a7c36f8
raidabad <123596928+raidabad@users.noreply.github.com>
2026-06-23 00:08:14 +0300

    Delete db_manager.py

--- db_manager.py
+++
@@ -1,8 +0,0 @@
-def get_products():
-    try:
-        # قمنا بإضافة .schema("public") لنضمن أننا نقرأ من الجدول الصحيح
-        response = supabase.table("products").select("*").execute()
-        return response.data
-    except Exception as e:
-        print(f"حدث خطأ: {e}")
-        return []
